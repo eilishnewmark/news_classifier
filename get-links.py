e6f671e826d4ff5206951cdfc9551304bc5911f0
eilishnewmark <eilish.newmark@icloud.com>
2023-09-29 15:33:06 +0100

    make json files, get-links.py update

--- get-links.py
+++ get-links.py
@@ -1,29 +1,23 @@
-from bs4 import BeautifulSoup
 from requests.auth import HTTPBasicAuth
 import requests
+import json
 
 auth = HTTPBasicAuth('apikey', '63581936-8fd7-40f7-ac80-76cb937c59a9')
-url = 'https://content.guardianapis.com/search?tag=politics/politics&api-key=63581936-8fd7-40f7-ac80-76cb937c59a9&page-size=50'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-response = requests.get(url, headers=headers, auth=auth)
 
-print(response.text)
+tags = ["environment"]
+# , "politics", "technology", "science", "society", "football", "food"
+
+for tag in tags:
+    from_date = "2023-09-01" # yyyy-mm-dd
+    to_date = "2023-09-22"
+    api_key = "63581936-8fd7-40f7-ac80-76cb937c59a9"
+    url = f"https://content.guardianapis.com/search?tag={tag}/{tag}&api-key={api_key}&from-date={from_date}&to-date={to_date}&page-size=50"
+    response = requests.get(url, headers=headers, auth=auth)
+    with open(f"./responses/{tag}.json", "w") as f:
+        json.dump(response.json(), f)
 
 
 
-# "sectionName":"Environment"
-# "webTitle":"Lords to debate mandating swift bricks in new homes in England"
-# "webUrl":"https://www.theguardian.com/environment/2023/sep/04/lords-to-debate-mandating-swift-bricks-in-new-uk-homes"
-
-
-
-# # data is used to store the text of the HTML document of the site
-# data = response.text
-# # soup parses that text file
-# soup = BeautifulSoup(data, "json.parser")
-
-# print(soup.prettify())
-
-# # get news article links
-# for link in soup.find_all("a", {"class":"link d-block p-relative card__image-container"}):
-#     print(link.get("href"))
+    
+        
