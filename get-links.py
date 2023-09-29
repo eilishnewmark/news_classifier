8a3496a754bf7a66fac9d65379aee3d1092b8546
eilishnewmark <eilish.newmark@icloud.com>
2023-09-29 15:39:29 +0100

    add more responses july(3)-aug(2)-sept(1/-)

--- get-links.py
+++ get-links.py
@@ -5,16 +5,17 @@ import json
 auth = HTTPBasicAuth('apikey', '63581936-8fd7-40f7-ac80-76cb937c59a9')
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
-tags = ["environment"]
-# , "politics", "technology", "science", "society", "football", "food"
+tags = ["environment" , "politics", "technology", "science", "society", "football", "food"]
+
 
 for tag in tags:
-    from_date = "2023-09-01" # yyyy-mm-dd
-    to_date = "2023-09-22"
+    i = 3
+    from_date = "2023-07-01" # yyyy-mm-dd
+    to_date = "2023-07-31"
     api_key = "63581936-8fd7-40f7-ac80-76cb937c59a9"
     url = f"https://content.guardianapis.com/search?tag={tag}/{tag}&api-key={api_key}&from-date={from_date}&to-date={to_date}&page-size=50"
     response = requests.get(url, headers=headers, auth=auth)
-    with open(f"./responses/{tag}.json", "w") as f:
+    with open(f"./responses/{tag}{i}.json", "w") as f:
         json.dump(response.json(), f)
 
 
