1dbb7b9784c7ebe10f0439c08baa5b4e7f1a1fa8
eilishnewmark <eilish.newmark@icloud.com>
2023-10-02 13:19:02 +0100

    start coding model and process data

--- model.py
+++ model.py
@@ -1,50 +1,27 @@
-from collections import Counter
+from utils import split_data, get_vocabs
+import torch
+import torch.nn as nn
 
-def get_vocabs(title_txtfile, tag_txtfile, title_vocab_outfile, tag_vocab_outfile):
-    with open(title_txtfile, "r") as titles:
-        with open(tag_txtfile, "r") as tags:
-            title_data = titles.readlines()
-            tag_data = tags.readlines()
-    
-    # get vocab set and size from all titles
-    stripped_lowered_titles = [title.lower().strip() for title in title_data]
-    all_tokens = []
-    for title in stripped_lowered_titles:
-         split_title = title.split()
-         for token in split_title:
-              all_tokens.append(token)   
-    vocab = list(set(all_tokens))
-    vocab_size = len(vocab)
+def load_data(title_fpath, tag_fpath):
+    title_train, title_test, tag_train, tag_test = split_data(title_fpath, tag_fpath)
 
-    # get tag set and size from all tags
-    stripped_tags = [tag.strip() for tag in tag_data]
-    tag_vocab = list(set(stripped_tags))
-    tag_vocab_size = len(tag_vocab)
-    
-    # get sorted tuple list of token and tag counts in the data
-    vocab_counts = Counter(all_tokens).most_common()
-    tag_counts = Counter(stripped_tags).most_common()
+    token2idx, tag2idx, idx2tag, vocab_size, tag_vocab_size, vocab_counts, tag_counts = get_vocabs(title_train, tag_train)
 
-    # write title and tag vocabs and counts to text files
-    with open(title_vocab_outfile, "w") as title_outfile:
-        with open(tag_vocab_outfile, "w") as tag_outfile:
-            for token_count in vocab_counts:
-                token, count = token_count
-                title_outfile.write(f"{token}\t{count}")
-                title_outfile.write("\n")
-            for tag_count in tag_counts:
-                    tag, count = tag_count
-                    tag_outfile.write(f"{tag}\t{count}")
-                    tag_outfile.write("\n")
+    batch_size = 32
+    n_iters = 280
+    epochs = n_iters / (len(title_train) / batch_size) # 10
 
-    # get dictionaries of token/tag to idx mapping for one hot vectors
-    title_indices = [i for i in range(0, vocab_size)]
-    token2idx = {token:idx for (token, idx) in zip(vocab, title_indices)}
-    tag_indices = [i for i in range(0, tag_vocab_size)]
-    tag2idx = {tag:idx for (tag, idx) in zip(tag_vocab, tag_indices)}
-    idx2tag = {idx:tag for (tag, idx) in tag2idx.items()} 
+    train_loader = torch.utils.data.DataLoader(dataset=title_train, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=title_test, batch_size=batch_size, shuffle=True)
 
-    return token2idx, tag2idx, idx2tag, vocab_size, tag_vocab_size
+load_data("data/tokenised_titles_without_punctuation.txt", "data/tags.txt")
 
-get_vocabs("tokenised_titles_without_punctuation.txt", "tags.txt", "vocabs/title_vocab.txt", "vocabs/tag_vocab.txt")
+class FFNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        self.input_linear = nn.Linear(input_dim, hidden_dim)
+        self.non_linear = nn.Tanh()
+        self.output_linear = nn.Linear(hidden_dim, output_dim)
+        self.softmax = nn.Softmax()
 
+    def forward(self, x):
+        pass
---
+++ utils.py
@@ -0,0 +1,41 @@
+from collections import Counter
+from sklearn.model_selection import train_test_split
+
+def split_data(title_fpath, tag_fpath):
+     with open(title_fpath, "r") as title_data:
+          with open(tag_fpath, "r") as tag_data:
+               titles = title_data.readlines()
+               tags = tag_data.readlines()
+     title_train, title_test, tag_train, tag_test = train_test_split(titles, tags, train_size=0.8)
+
+     return title_train, title_test, tag_train, tag_test
+
+def get_vocabs(titles, tags):
+    """titles/tags = list of title/tag strings"""
+    # get vocab set and size from all titles
+    stripped_lowered_titles = [title.lower().strip() for title in titles]
+    all_tokens = []
+    for title in stripped_lowered_titles:
+         split_title = title.split()
+         for token in split_title:
+              all_tokens.append(token)   
+    vocab = list(set(all_tokens))
+    vocab_size = len(vocab)
+
+    # get tag set and size from all tags
+    stripped_tags = [tag.strip() for tag in tags]
+    tag_vocab = list(set(stripped_tags))
+    tag_vocab_size = len(tag_vocab)
+    
+    # get sorted tuple list of token and tag counts in the data
+    vocab_counts = Counter(all_tokens).most_common()
+    tag_counts = Counter(stripped_tags).most_common()
+
+    # get dictionaries of token/tag to idx mapping for one hot vectors
+    title_indices = [i for i in range(0, vocab_size)]
+    token2idx = {token:idx for (token, idx) in zip(vocab, title_indices)}
+    tag_indices = [i for i in range(0, tag_vocab_size)]
+    tag2idx = {tag:idx for (tag, idx) in zip(tag_vocab, tag_indices)}
+    idx2tag = {idx:tag for (tag, idx) in tag2idx.items()} 
+
+    return token2idx, tag2idx, idx2tag, vocab_size, tag_vocab_size, vocab_counts, tag_counts
