505954049d003c7eeaaaebade01f18c1c9e3f49c
eilishnewmark <eilish.newmark@icloud.com>
2023-10-02 12:19:07 +0100

    model.py update to create tag vocab and title vocab

--- model.py
+++ model.py
@@ -8,26 +8,32 @@ def get_vocabs(title_txtfile, tag_txtfile, title_vocab_outfile, tag_vocab_outfil
     
     # get vocab set and size from all titles
     stripped_lowered_titles = [title.lower().strip() for title in title_data]
-    all_tokens = [title.split() for title in stripped_lowered_titles]
-    vocab = [token for token in all_tokens if token not in vocab]
+    all_tokens = []
+    for title in stripped_lowered_titles:
+         split_title = title.split()
+         for token in split_title:
+              all_tokens.append(token)   
+    vocab = list(set(all_tokens))
     vocab_size = len(vocab)
 
     # get tag set and size from all tags
     stripped_tags = [tag.strip() for tag in tag_data]
-    tag_vocab = [tag for tag in stripped_tags if tag not in tag_vocab]
+    tag_vocab = list(set(stripped_tags))
     tag_vocab_size = len(tag_vocab)
     
-    # get sorted dictionary of token and tag counts in the data
+    # get sorted tuple list of token and tag counts in the data
     vocab_counts = Counter(all_tokens).most_common()
     tag_counts = Counter(stripped_tags).most_common()
 
     # write title and tag vocabs and counts to text files
     with open(title_vocab_outfile, "w") as title_outfile:
         with open(tag_vocab_outfile, "w") as tag_outfile:
-            for token, count in vocab_counts.items():
+            for token_count in vocab_counts:
+                token, count = token_count
                 title_outfile.write(f"{token}\t{count}")
                 title_outfile.write("\n")
-            for tag, count in tag_counts.items():
+            for tag_count in tag_counts:
+                    tag, count = tag_count
                     tag_outfile.write(f"{tag}\t{count}")
                     tag_outfile.write("\n")
 
@@ -40,4 +46,5 @@ def get_vocabs(title_txtfile, tag_txtfile, title_vocab_outfile, tag_vocab_outfil
 
     return token2idx, tag2idx, idx2tag, vocab_size, tag_vocab_size
 
-get_vocabs("tokenised_titles.txt", "tags.txt", "vocabs/title_vocab.txt", "vocabs/tag_vocab.txt")
+get_vocabs("tokenised_titles_without_punctuation.txt", "tags.txt", "vocabs/title_vocab.txt", "vocabs/tag_vocab.txt")
+
