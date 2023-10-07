c33cf4c322beb11855f91217a7c57676cc7ee40e
eilishnewmark <eilish.newmark@icloud.com>
2023-10-07 17:47:01 +0100

    build up bigger dataset for training

--- get-links.py
+++ get-links.py
@@ -8,9 +8,9 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleW
 tags = ["environment" , "politics", "technology", "science", "society", "football", "food"]
 
 for tag in tags:
-    i = 3
-    from_date = "2023-07-01" # yyyy-mm-dd
-    to_date = "2023-07-31"
+    i = 10
+    from_date = "2022-12-01" # yyyy-mm-dd
+    to_date = "2022-12-31"
     api_key = "63581936-8fd7-40f7-ac80-76cb937c59a9"
     url = f"https://content.guardianapis.com/search?tag={tag}/{tag}&api-key={api_key}&from-date={from_date}&to-date={to_date}&page-size=50"
     response = requests.get(url, headers=headers, auth=auth)
